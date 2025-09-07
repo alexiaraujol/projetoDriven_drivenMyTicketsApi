f7faa73dd2c7fa8fb7642cadbd6038a5d813cc21
Alexia A. de Lima <alexiaalima96@gmail.com>
2025-09-07 20:22:45 -0300

    C:/Program Files/Git/health teste done

---
+++ tests/example.spec.ts
@@ -0,0 +1,14 @@
+import app from "../src/index";
+import supertest from "supertest";
+
+const api = supertest(app);
+
+
+describe ("GET /health", () => {
+    it("should return status 200 and a message", async () => {
+
+        const {status, text} = await api.get ("/health")
+        expect(status).toBe(200);
+        expect(text).toBe(`I'm okay!`);
+    })
+})
